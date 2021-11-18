608f799a0cdda2eeb408ffc7035d233603c0659d
cuter97 <aguscastets@gmail.com>
2021-11-18 02:03:16 -0300

    animaciones con jquery

---
+++ desafio 12 y 13/animaciones.js
@@ -0,0 +1,34 @@
+const animacion1 = $('.animation1');
+const animacion2 = $('.animation2');
+const tarjeta = $('.card');
+
+animacion2.css({'display': 'none'});
+
+$( () => {
+
+    const loop = () => {
+        animacion1.fadeOut(2000, () => {
+            animacion2.fadeIn(2000, () => {
+                animacion2.fadeOut(2000, () => {
+                    animacion1.fadeIn(2000, () => {
+                        setTimeout(loop, 500);
+                    });
+                });
+            });
+        });
+    }
+    loop();
+    
+});                  
+
+let elementsArray = document.querySelectorAll(".card");
+
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    elementsArray.forEach(elem => {
+        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) 
+            elem.classList.add("inView");       
+    });
+}
+fadeIn();
